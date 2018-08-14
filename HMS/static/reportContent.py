b8a5a98cd0a4f989cfe71908729f5dccf5b6cab8
Yaw Botwe <ybotwe@gmail.com>
2018-08-14 10:18:33 +0000

    Add files via upload

---
+++ HMS/static/reportContent.py
@@ -0,0 +1,10 @@
+reportContent = {
+    "totRooms": "Total number of rooms in ",
+    "totStu": "Total number of students in ",
+    "totStuPaid": "Total number of students who have fully paid in ",
+    "totNotFullPaid": "Total number of students who have not fully paid ",
+    "totFullRooms": "Total number of fully occupied rooms in ",
+    "totNotFullRooms": "Total number of that are not fully occupied rooms in ",
+    "totMaleStu": "Total number of males in ",
+    "totFemStu": "Total number of females in ",
+}
---
+++ HMS/static/tourcontent.py
@@ -0,0 +1,36 @@
+tourContent = [
+    {
+        "id": 1,
+        "pic": "static/hostel_pics/hosanna.jpg",
+        "title": "Hosanna",
+        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in lectus auctor, consequat tellus vitae, feugiat urna. Etiam velit risus, porta eget nisi non, egestas posuere justo. Nam ornare felis sit amet vehicula accumsan. Quisque at rhoncus est, ullamcorper condimentum tellus.Proin facilisis, nisi non bibendum interdum, urna purus efficitur neque, volutpat ornare dui nunc et diam.Proin bibendum mauris ac mollis viverra. Nam tincidunt, purus sed bibendum suscipit, est ante eleifend elit, at accumsan augue diam et ligula. Proin accumsan sem non finibus interdum. Etiam placerat metus in dui mollis, id rutrum ligula imperdiet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed tincidunt varius eros ac dapibus. Sed volutpat a justo at lobortis. Integer eu nisl py awesome."
+    },
+
+    {
+        "id": 2,
+        "pic": "static/hostel_pics/dufie.jpg",
+        "title": "Dufie",
+        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in lectus auctor," +
+                 " consequat tellus vitae, feugiat urna. Etiam velit risus, porta eget nisi non, egestas posuere justo." +
+                 " Nam ornare felis sit amet vehicula accumsan. Quisque at rhoncus est, ullamcorper condimentum tellus. " +
+                 "Proin facilisis, nisi non bibendum interdum, urna purus efficitur neque, volutpat ornare dui nunc et diam." +
+                 " Proin bibendum mauris ac mollis viverra. Nam tincidunt, purus sed bibendum suscipit, est ante eleifend elit," +
+                 " at accumsan augue diam et ligula. Proin accumsan sem non finibus interdum. Etiam placerat metus in dui mollis," +
+                 " id rutrum ligula imperdiet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis" +
+                 " egestas. Sed tincidunt varius eros ac dapibus. Sed volutpat a justo at lobortis. Integer eu nisl felis."
+    },
+
+    {
+        "id": 3,
+        "pic": "static/hostel_pics/charlotte.jpg",
+        "title": "Charlotte",
+        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in lectus auctor," +
+                 " consequat tellus vitae, feugiat urna. Etiam velit risus, porta eget nisi non, egestas posuere justo." +
+                 " Nam ornare felis sit amet vehicula accumsan. Quisque at rhoncus est, ullamcorper condimentum tellus. " +
+                 "Proin facilisis, nisi non bibendum interdum, urna purus efficitur neque, volutpat ornare dui nunc et diam." +
+                 " Proin bibendum mauris ac mollis viverra. Nam tincidunt, purus sed bibendum suscipit, est ante eleifend elit," +
+                 " at accumsan augue diam et ligula. Proin accumsan sem non finibus interdum. Etiam placerat metus in dui mollis," +
+                 " id rutrum ligula imperdiet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis" +
+                 " egestas. Sed tincidunt varius eros ac dapibus. Sed volutpat a justo at lobortis. Integer eu nisl felis."
+    }
+]
